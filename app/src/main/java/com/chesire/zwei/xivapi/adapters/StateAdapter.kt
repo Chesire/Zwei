f34908c5726977b182054500973f8540b988a077
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-01 17:24:06 +0100

    wip: change state adapter to be shorter

--- app/src/main/java/com/chesire/zwei/xivapi/adapters/StateAdapter.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/adapters/StateAdapter.kt
@@ -14,14 +14,8 @@ class StateAdapter {
 
     @FromJson
     fun fromJson(value: Int): State {
-        return when (value) {
-            0 -> State.None
-            1 -> State.Adding
-            2 -> State.Cached
-            3 -> State.NotFound
-            4 -> State.Blacklist
-            5 -> State.Private
-            else -> throw JsonDataException("Unexpected State: $value")
-        }
+        return State.values()
+            .find { it.value == value }
+            ?: throw JsonDataException("Unexpected State: $value")
     }
 }
