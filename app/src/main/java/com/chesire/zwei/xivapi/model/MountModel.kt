10c45084c416aaccb280d41e690b61efe54051de
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-02 17:06:56 +0100

    wip: add model for retrieved mounts

--- app/src/main/java/com/chesire/zwei/xivapi/model/MountModel.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/model/MountModel.kt
@@ -4,21 +4,23 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class CompanionModel(
+data class MountModel(
     @Json(name = "ID")
     val id: Int,
     @Json(name = "Icon")
     val icon: String,
-    @Json(name = "IconID")
-    val iconId: Int,
     @Json(name = "IconSmall")
     val iconSmall: String,
-    @Json(name = "Name")
-    val name: String,
+    @Json(name = "Order")
+    val order: Int,
     @Json(name = "Url")
     val url: String,
+    @Json(name = "Name")
+    val name: String,
     @Json(name = "Description")
     val description: String,
+    @Json(name = "DescriptionEnhanced")
+    val descriptionEnhanced: String,
     @Json(name = "Tooltip")
     val tooltip: String
 )
