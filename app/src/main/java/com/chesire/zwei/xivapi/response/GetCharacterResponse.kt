79d7d0a58e0b838342b5175ced73a2d9c20bd90f
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-04 22:55:09 +0100

    wip: add the freecompany model to the getcharacterresponse

--- app/src/main/java/com/chesire/zwei/xivapi/response/GetCharacterResponse.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/response/GetCharacterResponse.kt
@@ -1,6 +1,7 @@
 package com.chesire.zwei.xivapi.response
 
 import com.chesire.zwei.xivapi.model.CharacterModel
+import com.chesire.zwei.xivapi.model.FreeCompanyModel
 import com.chesire.zwei.xivapi.model.InfoModel
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -9,6 +10,8 @@ import com.squareup.moshi.JsonClass
 data class GetCharacterResponse(
     @Json(name = "Character")
     val character: CharacterModel?,
+    @Json(name = "FreeCompany")
+    val freeCompany: FreeCompanyModel?,
     @Json(name = "Info")
     val info: InfoModel
 )
