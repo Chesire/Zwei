20357ad66a62fcd2da86b1f1bb18b66256e688ed
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-14 23:18:37 +0100

    wip: add helper method for the race and gender
    Add method to pull the enum by its value

--- app/src/main/java/com/chesire/zwei/xivapi/flags/Gender.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/flags/Gender.kt
@@ -1,6 +1,13 @@
 package com.chesire.zwei.xivapi.flags
 
 enum class Gender(val value: Int) {
+    Unknown(-1),
     Male(1),
-    Female(2)
+    Female(2);
+
+    companion object {
+        fun getGenderForValue(value: Int): Gender {
+            return Gender.values().find { it.value == value } ?: Unknown
+        }
+    }
 }
--- app/src/main/java/com/chesire/zwei/xivapi/flags/Race.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/flags/Race.kt
@@ -4,10 +4,17 @@ package com.chesire.zwei.xivapi.flags
  * Contains a list of races a character can be.
  */
 enum class Race(val value: Int) {
+    Unknown(-1),
     Hyur(1),
     Elezen(2),
     Lalafell(3),
     Miqote(4),
     Roegadyn(5),
-    AuRa(6)
+    AuRa(6);
+
+    companion object {
+        fun getRaceForValue(value: Int): Race {
+            return Race.values().find { it.value == value } ?: Unknown
+        }
+    }
 }
