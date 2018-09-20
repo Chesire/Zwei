dcb9981428245beb4ea0dbdc0774359ff929d720
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-20 23:15:34 +0100

    chore(detekt): fix some raised detekt issues

--- app/src/main/java/com/chesire/zwei/xivapi/Resource.kt
+++ app/src/main/java/com/chesire/zwei/xivapi/Resource.kt
@@ -6,16 +6,8 @@ data class Resource<out T>(
     val message: String = ""
 ) {
     companion object {
-        fun <T> success(data: T): Resource<T> {
-            return Resource(Status.SUCCESS, data)
-        }
-
-        fun <T> error(msg: String, data: T): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
-        }
-
-        fun <T> loading(data: T): Resource<T> {
-            return Resource(Status.LOADING, data)
-        }
+        fun <T> success(data: T): Resource<T> = Resource(Status.SUCCESS, data)
+        fun <T> error(msg: String, data: T): Resource<T> = Resource(Status.ERROR, data, msg)
+        fun <T> loading(data: T): Resource<T> = Resource(Status.LOADING, data)
     }
 }
