452acf4fb60582495ff1c7eb7c8701e8b2fbdf0b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-22 19:39:26 +0100

    WIP: add injection to current views to ensure it works

--- app/src/main/java/com/chesire/zwei/ZweiApplication.kt
+++ app/src/main/java/com/chesire/zwei/ZweiApplication.kt
@@ -1,12 +1,21 @@
 package com.chesire.zwei
 
+import android.app.Activity
 import android.app.Application
 import android.os.StrictMode
+import com.chesire.zwei.dagger.AppInjector
 import com.squareup.leakcanary.LeakCanary
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import timber.log.Timber
+import javax.inject.Inject
 
 @Suppress("Unused")
-class ZweiApplication : Application() {
+class ZweiApplication : Application(), HasActivityInjector {
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+
     override fun onCreate() {
         super.onCreate()
 
@@ -21,7 +30,11 @@ class ZweiApplication : Application() {
             Timber.plant(Timber.DebugTree())
             startStrictMode()
         }
+
+        AppInjector.init(this)
     }
+
+    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
 
     private fun startStrictMode() {
         StrictMode.setThreadPolicy(
