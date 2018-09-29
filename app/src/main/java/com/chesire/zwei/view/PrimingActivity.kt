bc6d15bda60efe24c2cf922308b2779175b3bedc
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-29 18:19:58 +0100

    feat: add switch to the priming activity
    Add check for if onboarding should be displayed, choose which activity to load based on that. Currently it will load the onboarding activity until another one is added

--- app/src/main/java/com/chesire/zwei/view/PrimingActivity.kt
+++ app/src/main/java/com/chesire/zwei/view/PrimingActivity.kt
@@ -3,13 +3,24 @@ package com.chesire.zwei.view
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.chesire.zwei.dagger.Injectable
+import com.chesire.zwei.util.PrefHelper
 import com.chesire.zwei.view.onboarding.OnboardingActivity
+import javax.inject.Inject
 
-class PrimingActivity : Activity() {
+class PrimingActivity : Activity(), Injectable {
+    @Inject
+    lateinit var prefHelper: PrefHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Later will add a check if onboarding has been complete
-        startActivity(Intent(this, OnboardingActivity::class.java))
+
+        if (prefHelper.shouldDisplayOnboarding) {
+            startActivity(Intent(this, OnboardingActivity::class.java))
+        } else {
+            // start the primary activity - for now we only have onboarding
+            startActivity(Intent(this, OnboardingActivity::class.java))
+        }
         finish()
     }
 }
