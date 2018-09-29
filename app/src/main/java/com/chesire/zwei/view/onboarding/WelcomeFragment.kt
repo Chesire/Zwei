4aedd3c1c35878e789698b17ff70f3b24a7638ec
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-09-29 15:06:10 +0100

    WIP: remove the debug code from welcome

--- app/src/main/java/com/chesire/zwei/view/onboarding/WelcomeFragment.kt
+++ app/src/main/java/com/chesire/zwei/view/onboarding/WelcomeFragment.kt
@@ -1,46 +1,19 @@
 package com.chesire.zwei.view.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.chesire.zwei.R
 import com.chesire.zwei.dagger.Injectable
-import com.chesire.zwei.xivapi.XIVApi
-import kotlinx.android.synthetic.main.fragment_welcome.testButton
-import kotlinx.coroutines.experimental.launch
-import timber.log.Timber
-import javax.inject.Inject
 
 class WelcomeFragment : Fragment(), Injectable {
-    @Inject
-    lateinit var xivApi: XIVApi
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_welcome, container, false)
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        testButton.setOnClickListener {
-            launch {
-                try {
-                    val data = xivApi.searchForCharacter("Cheshire Cat", "Phoenix").await()
-
-                    Timber.e("INCOMING DATA")
-                    Timber.e("Name: ${data.data}.name")
-                    Timber.e("Message: ${data.message}")
-                    Timber.e("Found: ${data.data.count()} characters")
-                } catch (ex: Exception) {
-                    Timber.e(ex)
-                }
-            }
-        }
-    }
 
     companion object {
         const val tag = "WelcomeFragment"
