72fe38e182da59586e300400c84b6664b3c1922e
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-10-03 21:42:43 +0100

    test: update the testrunner for dexopener

--- app/src/androidTest/java/com/chesire/zwei/MockTestRunner.kt
+++ app/src/androidTest/java/com/chesire/zwei/MockTestRunner.kt
@@ -3,13 +3,16 @@ package com.chesire.zwei
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import com.github.tmurakami.dexopener.DexOpener
 
+@Suppress("unused")
 class MockTestRunner : AndroidJUnitRunner() {
     override fun newApplication(
         cl: ClassLoader?,
         className: String?,
         context: Context?
     ): Application {
-        return super.newApplication(cl, MockApplication::class.java.name, context)
+        DexOpener.install(this)
+        return super.newApplication(cl, "com.chesire.zwei.MockApplication", context)
     }
 }
