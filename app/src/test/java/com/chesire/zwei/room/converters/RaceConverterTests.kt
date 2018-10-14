1dee848a1779765d5254da71f823b73bc0278ab8
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-10-14 11:12:36 +0100

    test: convert to use spek
    Convert the RaceConverterTests to use Spek.

--- app/src/test/java/com/chesire/zwei/room/converters/RaceConverterTests.kt
+++ app/src/test/java/com/chesire/zwei/room/converters/RaceConverterTests.kt
@@ -1,19 +1,25 @@
 package com.chesire.zwei.room.converters
 
 import com.chesire.zwei.xivapi.flags.Race
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert
-import org.junit.Test
 
-class RaceConverterTests {
-    private val converter = RaceConverter()
+class RaceConverterTests : Spek({
+    val converter by memoized { RaceConverter() }
+    val expectedInt = 1
+    val expectedRace = Race.getRaceForValue(1)
 
-    @Test
-    fun `can convert race into int`() {
-        Assert.assertEquals(1, converter.fromRaceToInt(Race.getRaceForValue(1)))
+    given("a Race") {
+        it("can convert into an int") {
+            Assert.assertEquals(expectedInt, converter.fromRaceToInt(expectedRace))
+        }
     }
 
-    @Test
-    fun `can convert int into race`() {
-        Assert.assertEquals(Race.getRaceForValue(1), converter.fromIntToRace(1))
+    given("an int") {
+        it("can convert into a Gender") {
+            Assert.assertEquals(expectedRace, converter.fromIntToRace(expectedInt))
+        }
     }
-}
+})
