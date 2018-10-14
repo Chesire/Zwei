9f6e9ea6db3666be31835baa458e3dde4b58c2db
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2018-10-14 01:05:40 +0100

    test: convert to use spek
    Convert the GenderConverterTests to use Spek.

--- app/src/test/java/com/chesire/zwei/room/converters/GenderConverterTests.kt
+++ app/src/test/java/com/chesire/zwei/room/converters/GenderConverterTests.kt
@@ -1,19 +1,25 @@
 package com.chesire.zwei.room.converters
 
 import com.chesire.zwei.xivapi.flags.Gender
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert
-import org.junit.Test
 
-class GenderConverterTests {
-    private val converter = GenderConverter()
+class GenderConverterTests : Spek({
+    val converter by memoized { GenderConverter() }
+    val expectedInt = 1
+    val expectedGender = Gender.getGenderForValue(1)
 
-    @Test
-    fun `can convert gender into int`() {
-        Assert.assertEquals(1, converter.fromGenderToInt(Gender.getGenderForValue(1)))
+    given("a Gender") {
+        it("can convert into an int") {
+            Assert.assertEquals(expectedInt, converter.fromGenderToInt(expectedGender))
+        }
     }
 
-    @Test
-    fun `can convert int into gender`() {
-        Assert.assertEquals(Gender.getGenderForValue(1), converter.fromIntToGender(1))
+    given("an int") {
+        it("can convert into a Gender") {
+            Assert.assertEquals(expectedGender, converter.fromIntToGender(expectedInt))
+        }
     }
-}
+})
