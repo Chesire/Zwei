44e60fa0016b4f9a2e446654917e8ef806da517f
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-02-09 22:39:18 +0000

    refactor: inject coroutine context and use that

--- app/src/main/java/com/chesire/zwei/view/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/chesire/zwei/view/onboarding/OnboardingViewModel.kt
@@ -3,6 +3,7 @@ package com.chesire.zwei.view.onboarding
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.chesire.zwei.util.IOContext
 import com.chesire.zwei.view.SingleLiveEvent
 import com.chesire.zwei.view.onboarding.character.GetCharacterStatus
 import com.chesire.zwei.xivapi.Status
@@ -10,15 +11,21 @@ import com.chesire.zwei.xivapi.XIVApi
 import com.chesire.zwei.xivapi.model.CharacterDetailModel
 import com.chesire.zwei.xivapi.model.InfoModel
 import com.chesire.zwei.xivapi.model.SearchCharacterModel
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.concurrent.TimeoutException
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class OnboardingViewModel @Inject constructor(
-    private val xivApi: XIVApi
+    private val xivApi: XIVApi,
+    @IOContext private val ioContext: CoroutineContext
 ) : ViewModel() {
+    private val job = Job()
+    private val uiScope = CoroutineScope(job + Dispatchers.Main)
     private val _searchStatus = SingleLiveEvent<Status>()
     private val _getCharacterStatus = SingleLiveEvent<GetCharacterStatus>()
     private val _foundCharacters = MutableLiveData<List<SearchCharacterModel>>()
@@ -33,72 +40,80 @@ class OnboardingViewModel @Inject constructor(
     val foundCharacters: LiveData<List<SearchCharacterModel>>
         get() = _foundCharacters
 
-    fun searchForCharacter() = GlobalScope.launch {
-        if (characterName.value == null || world.value == null) {
+    fun searchForCharacter() {
+        if (characterName.value.isNullOrBlank() || world.value.isNullOrBlank()) {
             _searchStatus.value = Status.Error
-            return@launch
+            return
         }
 
         _searchStatus.value = Status.Loading
+        uiScope.launch(ioContext) {
+            try {
+                val result = xivApi.searchForCharacter(characterName.value!!, world.value!!).await()
 
-        try {
-            val searchRequest = xivApi.searchForCharacter(characterName.value!!, world.value!!)
-            val result = searchRequest.await()
-
-            when (result.status) {
-                Status.Error -> _searchStatus.value = Status.Error
-                Status.Success -> if (result.data.isEmpty()) {
-                    Timber.d("Could not find character - failure no data")
-                    _searchStatus.value = Status.Error
-                } else {
-                    Timber.d("Found ${result.data.count()} characters")
-                    _searchStatus.value = Status.Success
-                    _foundCharacters.value = result.data
-                    currentCharacter.value = result.data.first()
+                when (result.status) {
+                    Status.Error -> _searchStatus.postValue(Status.Error)
+                    Status.Success -> {
+                        if (result.data.isEmpty()) {
+                            Timber.d("Could not find character - failure no data")
+                            _searchStatus.postValue(Status.Error)
+                        } else {
+                            Timber.d("Found ${result.data.count()} characters")
+                            _searchStatus.postValue(Status.Success)
+                            _foundCharacters.postValue(result.data)
+                            currentCharacter.postValue(result.data.first())
+                        }
+                    }
+                    else -> {
+                        Timber.w("Got unexpected branch ${result.status}")
+                        _searchStatus.postValue(Status.Error)
+                    }
                 }
-                else -> {
-                    Timber.w("Got unexpected branch ${result.status}")
-                    _searchStatus.value = Status.Error
-                }
+            } catch (e: TimeoutException) {
+                Timber.e("Operation timeout $e")
+                _searchStatus.postValue(Status.Error)
             }
-        } catch (e: TimeoutException) {
-            Timber.e("Operation timeout $e")
-            _searchStatus.value = Status.Error
         }
     }
 
-    @Suppress("OptionalWhenBraces")
-    fun getCharacter() = GlobalScope.launch {
+    fun getCharacter() {
         if (currentCharacter.value == null) {
             _getCharacterStatus.value = GetCharacterStatus.Error
-            return@launch
+            return
         }
 
         _getCharacterStatus.value = GetCharacterStatus.Loading
 
-        try {
-            val getRequest = xivApi.getCharacter(currentCharacter.value!!.id)
-            val result = getRequest.await()
+        uiScope.launch(ioContext) {
+            try {
+                val result = xivApi.getCharacter(currentCharacter.value!!.id).await()
 
-            when {
-                result.status == Status.Error -> {
-                    _getCharacterStatus.value = GetCharacterStatus.Error
+                when (result.status) {
+                    Status.Error -> _getCharacterStatus.postValue(GetCharacterStatus.Error)
+                    Status.Success -> {
+                        when (result.data) {
+                            is InfoModel -> {
+                                _getCharacterStatus.postValue(GetCharacterStatus.GotInfo)
+                            }
+                            is CharacterDetailModel -> {
+                                _getCharacterStatus.postValue(GetCharacterStatus.GotCharacter)
+                            }
+                        }
+                    }
+                    else -> {
+                        Timber.w("Got unexpected branch ${result.status} with data type ${result.data.javaClass}")
+                        _getCharacterStatus.postValue(GetCharacterStatus.Error)
+                    }
                 }
-                result.status == Status.Success && result.data is InfoModel -> {
-                    _getCharacterStatus.value = GetCharacterStatus.GotInfo
-                }
-                result.status == Status.Success && result.data is CharacterDetailModel -> {
-                    // save to the DB / Repository
-                    _getCharacterStatus.value = GetCharacterStatus.GotCharacter
-                }
-                else -> {
-                    Timber.w("Got unexpected branch ${result.status} with data type ${result.data.javaClass}")
-                    _getCharacterStatus.value = GetCharacterStatus.Error
-                }
+            } catch (e: TimeoutException) {
+                Timber.e("Operation timeout $e")
+                _getCharacterStatus.postValue(GetCharacterStatus.Error)
             }
-        } catch (e: TimeoutException) {
-            Timber.e("Operation timeout $e")
-            _getCharacterStatus.value = GetCharacterStatus.Error
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
     }
 }
