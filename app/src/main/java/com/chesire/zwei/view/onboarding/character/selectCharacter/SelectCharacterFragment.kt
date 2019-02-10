f99a4d9439917b0b2962840f79270588093335c5
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-02-10 11:58:25 +0000

    feat: add skeleton adapter for the selectcharacter
    Add a basic adapter to display the characters that are found in
    the `SelectCharacterFragment`.

--- app/src/main/java/com/chesire/zwei/view/onboarding/character/selectCharacter/SelectCharacterFragment.kt
+++ app/src/main/java/com/chesire/zwei/view/onboarding/character/selectCharacter/SelectCharacterFragment.kt
@@ -5,20 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.chesire.zwei.R
-import com.chesire.zwei.databinding.FragmentSelectCharacterBinding
 import com.chesire.zwei.view.onboarding.OnboardingViewModel
 import com.chesire.zwei.view.onboarding.character.CharacterInteractor
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_select_character.fragmentSelectCharacterCharacterList
 import javax.inject.Inject
 
 class SelectCharacterFragment : DaggerFragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private var characterInteractor: CharacterInteractor? = null
+    private lateinit var viewAdapter: SelectCharacterViewAdapter
     private val viewModel: OnboardingViewModel by lazy {
         ViewModelProviders
             .of(requireActivity(), viewModelFactory)
@@ -36,16 +39,25 @@ class SelectCharacterFragment : DaggerFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return DataBindingUtil.inflate<FragmentSelectCharacterBinding>(
-            inflater,
-            R.layout.fragment_select_character,
-            container,
-            false
-        ).apply {
-            lifecycleOwner = viewLifecycleOwner
-            //vm = viewModel
-        }.root
+    ): View? = inflater.inflate(R.layout.fragment_select_character, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewAdapter = SelectCharacterViewAdapter()
+
+        fragmentSelectCharacterCharacterList.apply {
+            adapter = viewAdapter
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
+        }
+
+        viewModel.foundCharacters.observe(
+            viewLifecycleOwner,
+            Observer { characters ->
+                viewAdapter.setCharacters(characters)
+            }
+        )
     }
 
     companion object {
---
+++ app/src/main/java/com/chesire/zwei/view/onboarding/character/selectCharacter/SelectCharacterViewAdapter.kt
@@ -0,0 +1,52 @@
+package com.chesire.zwei.view.onboarding.character.selectCharacter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.chesire.zwei.R
+import com.chesire.zwei.view.GlideApp
+import com.chesire.zwei.xivapi.model.SearchCharacterModel
+import kotlinx.android.synthetic.main.adapter_item_search_character.view.adapterItemSearchCharacterAvatar
+import kotlinx.android.synthetic.main.adapter_item_search_character.view.adapterItemSearchCharacterName
+import kotlinx.android.synthetic.main.adapter_item_search_character.view.adapterItemSearchCharacterServer
+
+class SelectCharacterViewAdapter : RecyclerView.Adapter<SelectCharacterViewAdapter.ViewHolder>() {
+    private val characters = mutableListOf<SearchCharacterModel>()
+
+    fun setCharacters(newCharacters: List<SearchCharacterModel>) {
+        characters.clear()
+        characters.addAll(newCharacters)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.adapter_item_search_character,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = characters.count()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(characters[position])
+    }
+
+    inner class ViewHolder(
+        private val view: View
+    ) : RecyclerView.ViewHolder(view) {
+
+        fun bind(model: SearchCharacterModel) {
+            view.adapterItemSearchCharacterName.text = model.name
+            view.adapterItemSearchCharacterServer.text = model.server
+            GlideApp.with(view)
+                .load(model.avatar)
+                // .placeholder()
+                // .error()
+                .into(view.adapterItemSearchCharacterAvatar)
+        }
+    }
+}
